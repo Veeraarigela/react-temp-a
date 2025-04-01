ca14bc1fa2aec711a1fc0c45862380c441751459
Veeraarigela <veeraarigela1234@gmial.com>
2025-04-01 10:28:26 +0530

    Add

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,12 @@ import App6 from "./components/App6";
 import App7 from "./components/App7";
 import App8 from "./components/App8";
 import App9 from "./components/App9";
+import App10 from "./components/App10";
+import App11 from "./components/App11";
+import App12 from "./components/App12";
+import App13 from "./components/App13";
+import App14 from "./components/App14";
+import App15 from "./components/App15";
 function App() {
   return (
     <BrowserRouter>
@@ -22,7 +28,13 @@ function App() {
         <Link to="app6">App6</Link>-
         <Link to="app7">App7</Link>-
         <Link to="app8">App8</Link>-
-        <Link to="app9">App9</Link>
+        <Link to="app9">App9</Link>-
+        <Link to="app10">App10</Link>-
+        <Link to="app11">App11</Link>-
+        <Link to="app12">App12</Link>-
+        <Link to="app13">App13</Link>-
+        <Link to="app14">App14</Link>-
+        <Link to="app15">App15</Link>
       </div>
       <hr></hr>
       <div>
@@ -37,6 +49,12 @@ function App() {
           <Route path="app7" element={<App7 />} />
           <Route path="app8" element={<App8 />} />
           <Route path="app9" element={<App9 />} />
+          <Route path="app10" element={<App10 />} />
+          <Route path="app11" element={<App11 />} />
+          <Route path="app12" element={<App12 />} />
+          <Route path="app13" element={<App13 />} />
+          <Route path="app14" element={<App14 />} />
+          <Route path="app15" element={<App15 />} />
         </Routes>
       </div>
     </BrowserRouter>
